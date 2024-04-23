8fab2abad235dd5539857f0410bdd512b425e2e0
Gabriela Guzmán <136855829+gabrielaguzmn@users.noreply.github.com>
2024-04-23 17:38:04 -0500

    set sudokuBoard as a default matrix and add gameStatus method (not finished yet)

--- src/main/java/com/example/sudoku/controller/GameController.java
+++ src/main/java/com/example/sudoku/controller/GameController.java
@@ -1,39 +1,62 @@
 package com.example.sudoku.controller;
 
+import com.example.sudoku.model.Sudoku;
 import com.example.sudoku.view.alert.AlertBox;
-import com.example.sudoku.view.alert.IAlertBox;
-import javafx.event.EventHandler;
+import com.example.sudoku.view.alert.GameStatusAlert;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 
 public class GameController {
 
     @FXML
     private GridPane sudokuBoard;
 
+    Sudoku sudoku = new Sudoku();
+
     @FXML
     public void initialize(){
+        String baby_blue, blue;
+        baby_blue = "#bcceff";
+        blue = "#def7ff";
+        Color color = Color.web(baby_blue);
+        Color colorcito = Color.web(blue);
         for (int i = 0; i < 9; i++){
             for (int j = 0; j < 9; j++){
                 TextField numTxt = new TextField();
+                numTxt.setPrefHeight(56);
+                numTxt.setPrefWidth(56);
+                numTxt.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.DOTTED, CornerRadii.EMPTY, new BorderWidths(1))));
+                numTxt.setAlignment(Pos.CENTER);
+                numTxt.setText(String.valueOf(sudoku.getSudokuBoard()[i][j]));
+                if (sudoku.getSudokuBoard()[i][j] > 0){
+                    numTxt.setEditable(false);
+                    numTxt.setBackground(new Background(new BackgroundFill(color, null, null)));
+                }
+                else{
+                    numTxt.setText("");
+                    numTxt.setBackground(new Background(new BackgroundFill(colorcito, null, null)));
+                }
                 sudokuBoard.add(numTxt, i, j);
-                onKeyReleased(numTxt);
+                onKeyReleased(numTxt, i, j);
             }
         }
     }
 
-    private void onKeyReleased(TextField numTxt) {
-        numTxt.setOnKeyReleased(new EventHandler<KeyEvent>() {
-            @Override
-            public void handle(KeyEvent keyEvent) {
-                String num = numTxt.getText();
-                if (!isValidNum(num)) {
-                    AlertBox alertBox = new AlertBox();
-                    alertBox.showMessage("Error","Has ingresado un caracter invalido," +
-                            "por favor intenta de nuevo");
-                }
+    private void onKeyReleased(TextField numTxt, int i, int j) {
+        numTxt.setOnKeyReleased(keyEvent -> {
+            String num = numTxt.getText();
+            if (!isValidNum(num)) {
+                AlertBox alertBox = new AlertBox();
+                alertBox.showMessage("Error","Has ingresado un caracter invalido," +
+                        "por favor intenta de nuevo");
+                numTxt.setText("");
+            }
+            else{
+                sudoku.setNumberInTableSudoku(Integer.parseInt(num) ,i, j);
+                gameStatus(sudoku.getSudokuBoard());
             }
         });
     }
@@ -42,4 +65,20 @@ public class GameController {
         return num.matches("[1-9]") && num.length() == 1;
     }
 
+    public void gameStatus(int[][] sudokuBoard){
+        boolean sudokuBoardFull = true;
+        outerloop:
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (sudokuBoard[i][j] == 0){
+                    sudokuBoardFull = false;
+                    break outerloop;
+                }
+            }
+        }
+        if (sudokuBoardFull){
+            GameStatusAlert gameStatusAlert = new GameStatusAlert();
+            gameStatusAlert.showMessage("VICTORIA", "Felicidades, has ganado");
+        }
+    }
 }
--- src/main/java/com/example/sudoku/model/Sudoku.java
+++ src/main/java/com/example/sudoku/model/Sudoku.java
@@ -1,17 +1,26 @@
 package com.example.sudoku.model;
 
 public class Sudoku implements ISudoku {
-    private int [][] tableSudoku;
+    private int [][] sudokuBoard;
 
     public Sudoku(){
-        this.tableSudoku = new int[9][9];
-        fillSudoku();
+        this.sudokuBoard = new int[][]{
+                {0,0,0,0,9,0,8,2,0},
+                {0,1,0,0,0,0,5,0,9},
+                {7,0,9,0,1,0,0,0,0},
+                {0,6,2,7,0,1,0,9,0},
+                {0,0,0,0,6,0,0,0,0},
+                {0,8,0,3,0,9,1,4,0},
+                {0,0,0,0,8,0,9,0,2},
+                {8,0,4,0,0,0,0,3,0},
+                {0,1,6,0,3,0,0,0,0}
+        };
     }
 
     @Override
     public Boolean isNumberHorizontal(int number, int row) {
         for(int i=0; i<9; i++){
-            if(this.tableSudoku[row][i] == number){
+            if(this.sudokuBoard[row][i] == number){
                 return true;
             }
         }
@@ -21,7 +30,7 @@ public class Sudoku implements ISudoku {
     @Override
     public Boolean isNumberVertical(int number, int col) {
         for(int i=0; i<9; i++){
-            if(this.tableSudoku[i][col] == number){
+            if(this.sudokuBoard[i][col] == number){
                 return true;
             }
         }
@@ -30,26 +39,26 @@ public class Sudoku implements ISudoku {
 
     @Override
     public Boolean isNumberInGroupCell(int number, int row, int col) {
-        
+
         return true;
     }
 
-
-
-    private void fillSudoku(){
-        this.tableSudoku[0][3] = 9;
-    }
+//    private void fillSudoku(){
+//        this.sudokuBoard[0][3] = 9;
+//    }
 
     @Override
     public void setNumberInTableSudoku(int number, int row, int col) {
-        this.tableSudoku[number] [row] = number;
+        this.sudokuBoard[row][col] = number;
     }
 
-    public int[][] getTableSudoku() {
-        return tableSudoku;
+    public int[][] getSudokuBoard() {
+        return sudokuBoard;
     }
 
-    public void setTableSudoku(int[][] tableSudoku) {
-        this.tableSudoku = tableSudoku;
-    }
+//    public void setSudokuBoard(int[][] sudokuBoard){
+//        this.sudokuBoard = sudokuBoard;
+//    }
+
+
 }
